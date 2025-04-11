272a2cb17502224caf3f61b6139c23e3edc98346
prakash-v181 <prakashtare110@outlook.com>
2025-04-11 23:09:13 +0530

    paper

---
+++ contact-us.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>News Portal | Contact us</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="index.php"><img src="images/logo.png" height="50"></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="about-us.php">About</a>
+            </li>
+              <li class="nav-item">
+              <a class="nav-link" href="category.php?catid=9">Covid-19</a>
+            </li>
+			 <li class="nav-item">
+              <a class="nav-link" href="category.php?catid=6">Politics</a>
+            </li>
+			
+			 <li class="nav-item">
+              <a class="nav-link" href="category.php?catid=7">Business</a>
+            </li>
+			 <li class="nav-item">
+              <a class="nav-link" href="category.php?catid=7">International</a>
+            </li>
+			
+             <li class="nav-item">
+              <a class="nav-link" href="contact-us.php">Contact us</a>
+            </li>
+  
+  
+          </ul>
+        </div>
+      </div>
+    </nav>    <!-- Page Content -->
+    <div class="container">
+
+      <h1 class="mt-4 mb-3">Contact Details  
+      </h1>
+
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="index.php">Home</a>
+        </li>
+        <li class="breadcrumb-item active">Contact</li>
+      </ol>
+
+      <!-- Intro Content -->
+      <div class="row">
+
+        <div class="col-lg-12">
+
+          <p><p><br></p><p><b>Address : Bidar </b>karnataka India</p><p><b>Phone Number : </b>+91 -01234567890</p><p><b>Email -id : </b>prakash@gmail.com</p></p>
+        </div>
+      </div>
+      <!-- /.row -->
+    
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+     <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white"></p>
+      </div>
+      <!-- /.container -->
+    </footer>
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
